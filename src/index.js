fb4d8bf2594d55b8a0c5a885c872d031c760ba11
Діана Денисюк <denys_d@rivne.itstep.org>
2019-09-30 11:56:54 +0300

    create app + add api.js need add user interface

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+//API
+import APIClient from './APIClient/api';
+
+class App extends React.Component{
+    
+    state={
+        newsList: []
+    }
+
+    updateNews = (country = "ua") =>{
+        const news = new APIClient();
+          const newsList = news.getAllNews(country)
+          .then(({articles}) =>{
+              this.setState({
+                  newsList: articles
+              })
+          })
+          .catch(err => console.log(err.message));
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount");
+        this.updateNews();
+        //console.log("newState = ", this.state.newsList);
+      }
+    render(){
+        console.log("newState = ", this.state.newsList);
+        return(
+            <h1>News APP</h1>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
